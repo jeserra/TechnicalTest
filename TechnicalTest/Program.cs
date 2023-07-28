3777b548c5bb467045ff5858c81a806de2291f42
Jose Eduardo Serrano <joser_isi@hotmail.com>
2023-07-28 04:06:30 -0600

    Add setting in the web api project

--- TechnicalTest/Program.cs
+++ TechnicalTest/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using TechnicalTest.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+IConfiguration Configuration = new ConfigurationBuilder()
+    .SetBasePath(System.AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var connectionString = Configuration.GetConnectionString("PostgreSQL");
 // Add services to the container.
-builder.Services.AddDbContext<ProductsContext>();
+builder.Services.AddDbContext<ProductsContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
